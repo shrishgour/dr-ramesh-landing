98341729fd982d7a1dd4d20f80c718a0b26a07f1
shrishgour <shrishgour02071992@gmail.com>
2025-09-21 17:03:29 +0530

    clean up

--- components/navbar-02/themeButton.jsx
+++ components/navbar-02/themeButton.jsx
@@ -1,10 +1,18 @@
 "use client";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 
 function ThemeButton() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // render nothing on server
   return (
     <Button
       size="icon"
